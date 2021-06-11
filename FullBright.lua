81dac3d1e0ac92ef35005cd5a458d0edff25f658
Midwaey <71154041+Midwaey@users.noreply.github.com>
2021-06-11 14:28:28 -0500

    Update FullBright.lua

--- FullBright.lua
+++ FullBright.lua
@@ -3,21 +3,8 @@ if not _G.FullBrightExecuted then
 	_G.FullBrightEnabled = false
 
 	_G.NormalLightingSettings = {
-		Brightness = game:GetService("Lighting").Brightness,
 		ClockTime = game:GetService("Lighting").ClockTime,
 	}
-
-	game:GetService("Lighting"):GetPropertyChangedSignal("Brightness"):Connect(function()
-		if game:GetService("Lighting").Brightness ~= 1 and game:GetService("Lighting").Brightness ~= _G.NormalLightingSettings.Brightness then
-			_G.NormalLightingSettings.Brightness = game:GetService("Lighting").Brightness
-			if not _G.FullBrightEnabled then
-				repeat
-					wait()
-				until _G.FullBrightEnabled
-			end
-			game:GetService("Lighting").Brightness = 1
-		end
-	end)
 
 	game:GetService("Lighting"):GetPropertyChangedSignal("ClockTime"):Connect(function()
 		if game:GetService("Lighting").ClockTime ~= 12 and game:GetService("Lighting").ClockTime ~= _G.NormalLightingSettings.ClockTime then
@@ -31,7 +18,6 @@ if not _G.FullBrightExecuted then
 		end
 	end)
 
-	game:GetService("Lighting").Brightness = 1
 	game:GetService("Lighting").ClockTime = 12
 
 	local LatestValue = true
@@ -42,10 +28,8 @@ if not _G.FullBrightExecuted then
 		while wait() do
 			if _G.FullBrightEnabled ~= LatestValue then
 				if not _G.FullBrightEnabled then
-					game:GetService("Lighting").Brightness = _G.NormalLightingSettings.Brightness
 					game:GetService("Lighting").ClockTime = _G.NormalLightingSettings.ClockTime
 				else
-					game:GetService("Lighting").Brightness = 1
 					game:GetService("Lighting").ClockTime = 12
 				end
 				LatestValue = not LatestValue
